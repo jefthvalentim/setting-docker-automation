a5984bbc5d56788e2b1e56f31c55012e8515abae
Jefth Valentim <jefthvalentim@hotmail.com>
2023-10-17 09:16:06 -0400

    add setting code

---
+++ setting_docker.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+chmod +x install_docker.sh
+chmod +x install_portainer.sh
+chmod +x install_nginx_proxy_manager.sh
+chmod +x uninstall_docker.sh
+chmod +x install_docker_compose.sh
+
+# Display a message indicating successful installation
+echo "Ambiente configurado!."
+
